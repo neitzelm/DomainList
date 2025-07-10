a0336e122241bbca7e4990d22298119e34387c92
neitzelm <neitzelm@gmail.com>
2025-07-10 08:39:04 -0400

    Update tag-customer.js

--- api/tag-customer.js
+++ api/tag-customer.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Missing email or customerId' });
   }
 
-  const domain = email.split('@')[1].toLowerCase();
+  const domain = email;
 
   try {
     // Fetch the discount domain map
